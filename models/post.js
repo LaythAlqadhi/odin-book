6f1545ac7219bb3aa5839bd5bf0e6d1d9e3b60ee
LaythAlqadhi <125077045+LaythAlqadhi@users.noreply.github.com>
2024-01-13 18:46:56 +0000

    Add user and post models

---
+++ models/post.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const commentSchema = new Schema(
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: 'Post',
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true },
+);
+
+const postSchema = new Schema(
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    comments: [commentSchema],
+  },
+  { timestamps: true },
+);
+
+postSchema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('User', postSchema);
---
+++ models/user.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profile: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+      fullName: String,
+      avatar: String,
+    },
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    }],
+    requests: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    }],
+  },
+  { timestamps: true },
+);
+
+userSchema.pre('save', function updateFullName(next) {
+  this.profile.fullName = `${this.profile.firstName} ${this.profile.lastName}`;
+  next();
+});
+
+userSchema.pre('save', async function hashPassword(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+userSchema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('User', userSchema);
