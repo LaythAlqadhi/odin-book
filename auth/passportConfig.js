4df6e1fa08591e817c95453f9f42db9fbb5b2ea4
LaythAlqadhi <125077045+LaythAlqadhi@users.noreply.github.com>
2024-02-14 00:10:51 +0000

    Add github and JWT strategies

--- auth/passportConfig.js
+++ auth/passportConfig.js
@@ -1,16 +1,38 @@
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const JwtStrategy = require('passport-jwt').Strategy;
-const { ExtractJwt } = require('passport-jwt');
+const LocalStrategy = require('passport-local').Strategy;
 const GitHubStrategy = require('passport-github2').Strategy;
 const bcrypt = require('bcryptjs');
+const { ExtractJwt } = require('passport-jwt');
 
 const User = require('../models/user');
+
+// JWT strategy configuration
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+// Passport configuration
+passport.use(
+  new JwtStrategy(jwtOptions, async (jwtPayload, done) => {
+    try {
+      const user = await User.findById(jwtPayload.sub);
+
+      if (user) {
+        return done(null, user);
+      }
+      return done(null, false);
+    } catch (err) {
+      return done(err, false);
+    }
+  }),
+);
 
 passport.use(
   new LocalStrategy(async (username, password, done) => {
     try {
-      const user = await User.findOne({ username: username });
+      const user = await User.findOne({ username });
       if (!user) {
         return done(null, false, { message: 'Incorrect username' });
       }
@@ -24,41 +46,44 @@ passport.use(
     } catch (err) {
       return done(err);
     }
-  })
-);
-
-const jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.JWT_SECRET,
-};
-
-passport.use(
-  new JwtStrategy(jwtOptions, async (jwtPayload, done) => {
-    try {
-      const user = await User.findById(jwtPayload.sub).exec();
-
-      if (user) {
-        return done(null, user);
-      }
-      return done(null, false);
-    } catch (err) {
-      return done(err, false);
-    }
   }),
 );
 
 const githubOptions = {
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: 'https://b32a7bae-6556-4da3-a848-f0e0b80bf4f0-00-36mr5e3zsor9c.janeway.replit.dev/api/auth/github/callback'
-}
+  callbackURL: process.env.GUTHUB_CALLBACK_URL,
+};
 
-passport.use(new GitHubStrategy(githubOptions,
-  function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate({ githubId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
-  }
-));
+passport.use(
+  new GitHubStrategy(
+    githubOptions,
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        console.error(profile);
+        const defaultUser = {
+          githubId: profile.id,
+          username: profile.username,
+          email: profile._json.email,
+          profile: {
+            displayName: profile.displayName,
+            avatar: profile.photos[0].value,
+            bio: profile._json.bio,
+          },
+        };
+
+        let user = await User.findOne({ githubId: profile.id });
+
+        if (!user) {
+          user = await User.create(defaultUser);
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err, false);
+      }
+    },
+  ),
+);
 
 module.exports = passport;
